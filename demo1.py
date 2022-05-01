26a10e1aca768e63048a21e8b7bb8c1e2853550e
0nimasile <1324059693@qq.com>
2022-05-02 03:27:09 +0800

    添加异常处理

--- demo1.py
+++ demo1.py
@@ -5,5 +5,15 @@ import pymysql
     cursor = db.cursor()
     # SQL 插入语句
     sql = "INSERT INTO commodity(name, origin, price) VALUES ('%s', '%s', '%f')" % (name, origin, price)
+try:
+    # 执行sql语句
+    cursor.execute(sql)
+    # 提交到数据库执行
+    db.commit()
+except:
+    # 如果发生错误则回滚
+    db.rollback()
+# 关闭数据库连接
+db.close()
 
 
